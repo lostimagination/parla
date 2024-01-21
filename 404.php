eb4ab859e3787d080566cca0e672beafabb34941
lostimagination <diva.tk@ukr.net>
2024-01-21 16:35:38 +0100

    404 changed

--- 404.php
+++ 404.php
@@ -13,7 +13,7 @@ get_header();
 <div class="container">
 
 	<section class="not-found">
-		<h1 class="h3 not-found__title"><?php esc_html_e( 'Page not found.', 'parla' ); ?></h1>
+		<h1 class="h3 not-found__title"><?php esc_html_e( 'Page not found. Please try again', 'parla' ); ?></h1>
 		<p class="not-found__text"><?php esc_html_e( 'Sorry, the page you were looking for doesn\'t exist or has been moved.', 'parla' ); ?></p>
 		<a href="<?php echo home_url('/') ?>" class="btn btn-primary"><?php esc_html_e( 'Back to homepage', 'parla' ); ?></a>
 	</section>
